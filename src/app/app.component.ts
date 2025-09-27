02d3afadd541368c87e32fc1e5647fd1b8e389e2
JanmejayM <mohapatra.janmejayz@gmail.com>
2025-09-28 01:23:35 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quoteData from '../assets/offline_quotes.json';
+import platformData from '../assets/coding_platforms.json';
+import bgImgData from '../assets/bg_img/bg_img.json'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dev-motivator';
+export class AppComponent implements OnInit {
+  quotesArr:{quote:string,author:string}[]=[]
+  displayQuote:{quote:string,author:string}={quote:"",author:""};
+  color: string | undefined;
+  displayBasic: boolean =false;
+  platforms:any[]=[];
+  ngOnInit(): void {
+    this.platforms=platformData;
+    this.quotesArr=quoteData;
+    this.showRandomQuote();
+  }
+
+  showRandomQuote(){
+    const randomIndex=Math.floor(Math.random()*this.quotesArr.length);
+    this.displayQuote=this.quotesArr[randomIndex];
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { FormsModule } from '@angular/forms';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ChipsModule } from 'primeng/chips';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,OverlayPanelModule,ButtonModule,InputGroupModule,InputTextModule,ChipsModule,InputGroupModule,CommonModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
