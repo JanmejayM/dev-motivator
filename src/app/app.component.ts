e53eea172d53d5573e7b29bc7f9a358f253387b3
JanmejayM <mohapatra.janmejayz@gmail.com>
2025-09-28 09:43:47 +0530

    backgound walpaper toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,27 @@ import bgImgData from '../assets/bg_img/bg_img.json'
 export class AppComponent implements OnInit {
   quotesArr:{quote:string,author:string}[]=[]
   displayQuote:{quote:string,author:string}={quote:"",author:""};
-  color: string | undefined;
-  displayBasic: boolean =false;
+  BgImgList:any[]=[];
+  currentBg:any;
+  currentBgInd=0;
   platforms:any[]=[];
   ngOnInit(): void {
     this.platforms=platformData;
     this.quotesArr=quoteData;
+    this.BgImgList=bgImgData;
+    this.currentBg=this.BgImgList[0];
+    this.currentBgInd=0;
     this.showRandomQuote();
   }
 
   showRandomQuote(){
     const randomIndex=Math.floor(Math.random()*this.quotesArr.length);
     this.displayQuote=this.quotesArr[randomIndex];
+  }
+
+  changeWalpaper(){
+    this.currentBg=this.BgImgList[((this.currentBgInd+1)%this.BgImgList.length)]
+    this.currentBgInd++;
   }
   
 }
