a20240ade47544ca360a402abc76edc4c9d4f51d
JanmejayM <mohapatra.janmejayz@gmail.com>
2025-09-28 08:22:46 +0530

    remove primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ColorPickerModule } from 'primeng/colorpicker';
+
 import { FormsModule } from '@angular/forms';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { InputGroupModule } from 'primeng/inputgroup';
-import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { ChipsModule } from 'primeng/chips';
+
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,FormsModule,OverlayPanelModule,ButtonModule,InputGroupModule,InputTextModule,ChipsModule,InputGroupModule,CommonModule
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, CommonModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
